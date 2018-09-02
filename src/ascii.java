0f3b4f3d63bdd4d16c303aec39ed98623abe524c
mattb20 <matt_burgoyne@live.co.uk>
2018-09-02 16:36:16 +0100

    using regex for 10 digits

---
+++ src/ascii.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+class SubStringCons{
+    public static void main(String args[]) {
+        byte ascii[] = {65,66,67,68,69,70};
+
+        String s1 = new String(ascii);
+        System.out.println(s1);
+
+        String s2 = new String(ascii, 2, 3);
+        System.out.println(s2);
+
+        List<String> strings = Arrays.asList("this", "is", "a", "long", "list", "of", "strings", "to", "use", "as", "a", "demo");
+
+        System.out.println(hasTenDigits("0123456789"));
+    }
+
+    private static boolean hasTenDigits(String s) {
+        return s.matches(".*[0-9]{10}.*");
+    }
+}
+
